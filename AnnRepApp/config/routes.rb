34d662b3c605eba876f8fa9cad3327da5c19ac28
John S <alucinare@gmail.com>
2017-06-23 10:20:57 +1000

    Add nested routing

--- AnnRepApp/config/routes.rb
+++ AnnRepApp/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "reports#index"
 
-  resources :reports do
-    resources :parts
-    resources :sections
+  root to: "reports#index"
+  resources :users do
+    resources :reports do
+      resources :parts, :sections
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
